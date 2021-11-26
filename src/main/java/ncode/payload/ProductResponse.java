c08c60c9e3b15461885d576c0a8d6b6d85b17ea5
김승환 at Home <61crossroad@gmail.com>
2021-11-27 03:12:13 +0900

    create boilerplate

---
+++ src/main/java/ncode/payload/ProductResponse.java
@@ -0,0 +1,19 @@
+package ncode.payload;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class ProductResponse {
+
+    private Long id;
+    private String sku;
+    private String name;
+    private BigDecimal price;
+    private Integer quantity;
+    private Boolean liked; // 찜 여부
+    private Integer totalLiked; // 상품이 받은 모든 찜 개수
+    private Integer viewed; // 상품 조회 수
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+}
---
+++ src/main/resources/hsqldb/data.sql
@@ -0,0 +1,47 @@
+INSERT INTO `product` (`sku`, `name`, `price`, `quantity`)
+VALUES
+('4720110NPVV 1000', 'Black leather Opyum 110 pumps', 1770000.00, 10),
+('471894Y9901 1084', 'Black acetate sunglasses', 681000.00, 10),
+('464581YB2PG 9000', 'White cotton sweatshirt', 1122000.00, 10),
+('464581YB2PG 1000', 'Black cotton sweatshirt', 1122000.00, 10),
+('464572YB2DQ 1403', 'Melange grey cotton t-shirt', 591000.00, 10),
+('4609133Y201 1000', 'Black cashmere scarf', 771000.00, 10),
+('457142SIW20 9000', 'White cotton t-shirt', 375000.00, 10),
+('457142SIW20 1000', 'Black cotton t-shirt', 375000.00, 10),
+('4568178 A9393', 'Printed cotton trench coat', 4721000.00, 10),
+('4568116 A9115', 'Cerulean nylon trench coat', 3515000.00, 10),
+('4567918 A9240', 'Printed stretch nylon bodysuit', 1104000.00, 10),
+('4567729 A1880', 'Printed silk pant', 2975000.00, 10),
+('4567708 A9115', 'Cerulean nylon overalls', 2669000.00, 10),
+('4567640 A1464', 'White mesh t-shirt', 1248000.00, 10),
+('4567618 A5088', 'Printed silk shirt', 1662000.00, 10),
+('4567436 A9113', 'Embroidered cotton blend sweater', 2075000.00, 10),
+('4567391 A1464', 'Ivory gabardine pant', 2003000.00, 10),
+('4567264 A1189', 'Black mohair blend pant', 1841000.00, 10),
+('4566913Y001 1077', 'Printed silk foulard', 528000.00, 10),
+('4532760SX0E 1000', 'Black leather wallet', 681000.00, 10),
+('4532490SX0E 1000', 'Black leather iPad holder', 951000.00, 10),
+('452159BOW0J 1000', 'Black leather small Kate clutch', 2309000.00, 10),
+('448065DTD1T 5729', 'Antiqued pink leather mini GG Marmont crossbody bag', 2009000.00, 10),
+('447632DTD1T 1000', 'Black leather small GG Marmont crossbody bag', 2094000.00, 10),
+('446744DTDIT 9022', 'Ivory leather mini Marmont shoulder bag', 2926000.00, 10) ;
+
+INSERT INTO `product` (`sku`, `name`, `price`, `quantity`, `deleted_at`)
+VALUES
+('446744DTDIT 1000', 'Black leather mini GG Marmont shoulder bag', 2926000.00, 10, '2021-11-11 13:02:33'),
+('443436DTDI1 1000', 'Black leather GG Marmont wallet', 1160000.00, 10, '2021-11-11 13:02:33'),
+('443127DTDXP 3967', 'Multicolor leather Marmont card holder', 481000.00, 10, '2021-11-11 13:02:33'),
+('443127DTD1T 1000', 'Black leather Marmont card holder', 447000.00, 10, '2021-11-11 13:02:33'),
+('440222CWU01 1000', 'Black leather clutch', 1311000.00, 10, '2021-11-11 13:02:33'),
+('432831AQ41G 1000', 'Black leather small Classic City handbag', 3173000.00, 10, '2021-11-11 13:02:33'),
+('43194202JP0 9064', 'White leather Ace sneakers', 1025000.00, 10, '2021-11-11 13:02:33') ;
+
+INSERT INTO `user` (`email`, `name`, `deleted_at`)
+VALUES
+('user1@itsdcode.com', '회원1', '2021-10-10 16:11:23') ;
+
+INSERT INTO `user` (`email`, `name`)
+VALUES
+('user2@itsdcode.com', '회원2'),
+('user3@itsdcode.com', '회원3'),
+('user4@itsdcode.com', '회원4') ;
